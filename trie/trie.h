d930f1fd4e13ad3d9f7ff58e2f933673ff093eef
scp066 <60488077+scp066@users.noreply.github.com>
2020-12-06 01:47:10 +0000

    Add files via upload

---
+++ trie/trie.h
@@ -0,0 +1,34 @@
+#ifndef _TRIE_H_
+#define _TRIE_H_
+#include <vector>
+#include <string>
+class trie{
+    private:
+        *node root;
+        void destroy();
+        *node search(*node root, std::string& word);
+        void insert(*node root, std::string& word);
+
+    public:
+        trie();
+        ~trie();
+        void insert(std::string word);
+        *node search(std::string word);
+};
+
+
+
+class node{
+    private:
+        char key;
+        std::vector<*node> children;
+        int count;
+
+    public:
+        node();
+        node(char data);
+        ~node();
+    friend class trie;
+};
+
+#endif

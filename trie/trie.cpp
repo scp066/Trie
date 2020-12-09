d04fb4f15c2c19d1d59a9830c2dbfedb5a17a726
scp066 <60488077+scp066@users.noreply.github.com>
2020-12-09 02:46:44 +0000

    cleaned useless code

--- trie/trie.cpp
+++ trie/trie.cpp
@@ -3,13 +3,13 @@
 
 node::node(std::string word): key(word), count(1){
 	for(int i=0; i<26; i++){
+		// all trie functions rely on unused children being null
 		this->child[i]=nullptr;
 	}
 }
 
 
 node::~node(){
-
 
 }
 
@@ -33,11 +33,9 @@ void trie::destroy(node* root){
 }
 
 node* trie::insert(node* root, std::string& word, int depth = 1){
+	//This function will only be called if the node isnt in the trie
 	if(!root){
 		return new node(word);
-	}
-	if(root->key == word){
-		return root;
 	}
 	std::string fragment = word.substr(0,depth);
 	int index = fragment[depth-1]-97;
@@ -69,6 +67,7 @@ void trie::generatedot(node* root, std::ofstream& file){
 	for(int i=0; i<26; i++){
 		if(root->child[i]){
 			if(root->key == " "){
+				//Enclosed name of node in quotes to avoid conflicts with reserved words
 				file<<"\"ROOT\""<<" -- \""<<root->child[i]->key<<"\";\n";
 			}
 			else{
@@ -91,9 +90,11 @@ trie::~trie(){
 
 void trie::insert(std::string word){
 	changecase(word);
+	//if word is in trie don't insert it
 	node* t = search(this->root,word);
 	if(t){
 		t->count+=1;
+		return;
 	}
 	int length = word.length()+1;
 	for(int i=1; i<length; i++){
@@ -110,9 +111,9 @@ node* trie::search(std::string word){
 void trie::generatedot(std::string& fname){
 	std::ofstream graph(fname);
 	if(graph.is_open()){
-	graph<<"graph Trie{\n";
-	generatedot(this->root, graph);
-	graph<<"}";
-	graph.close();
+		graph<<"graph Trie{\n";
+		generatedot(this->root, graph);
+		graph<<"}";
+		graph.close();
 	}
 }

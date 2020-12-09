eb990c56baa35f67254caad1821a697cec9ab982
scp066 <60488077+scp066@users.noreply.github.com>
2020-12-09 05:00:14 +0000

    cleaned up code

--- trie/main.cpp
+++ trie/main.cpp
@@ -5,37 +5,47 @@
 #include "trie.h"
 
 
+void addtolist(const std::string& element, std::vector<std::string>& list){
+    bool inlist = false;
+    int size = list.size();
+    for(int i=0; i<size; i++){
+            if(element == list[i]){
+                inlist = true;
+                break;
+            }
+        }
+        if(!inlist){
+            list.push_back(element);
+        }
+}
+
+void searchtrie(trie& tree, const std::vector<std::string>& list){
+    int size = list.size();
+    for(int i=0; i<size; i++){
+        node* t = tree.search(list[i]);
+        std::cout<<"The word \""<<t->getkey()<<"\" is in the Trie "<<t->getcount()<<" times.\n";
+    }
+}
+
 
 int main(int argc, char *argv[]){
-    if(argc !=2){
+    if(argc !=3){
         std::cout<<"Usage:\n";
-        std::cout<<"./prog <file in>\n";
+        std::cout<<"./prog <file in> <dot file name>\n";
+        std::cout<<"Input files should only have letters and commas to separate the words!\n";
         return 0;
     }
     std::ifstream file;
     file.open(argv[1]);
     std::string element;
     trie tree;
-    std::string fname = "trie.gv";
+    std::string fname = argv[2];
     std::vector<std::string> searchlist;
     while(std::getline(file, element, ',')){
-        int size = searchlist.size();
-        bool inlist = false;
         tree.insert(element);
-        for(int i=0; i<size; i++){
-            if(element == searchlist[i]){
-                inlist = true;
-                break;
-            }
-        }
-        if(!inlist){
-            searchlist.push_back(element);
-        }
+        addtolist(element, searchlist);
     }
-    int size = searchlist.size();
-    for(int i=0; i<size; i++){
-        node* t = tree.search(searchlist[i]);
-        std::cout<<"The word \""<<t->getkey()<<"\" is in the Trie "<<t->getcount()<<" times.\n";
-    }
+    searchtrie(tree, searchlist);
     tree.generatedot(fname);
 }
+
